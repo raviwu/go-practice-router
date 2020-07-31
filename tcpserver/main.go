1a4b17842042715c58b7a0136b135a638ee89c4b
Ravi Wu <raviwu@gmail.com>
2020-07-31 11:09:06 +0800

    TCP server

---
+++ tcpserver/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer l.Close()
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(c, "connected")
+		c.Close()
+	}
+}
